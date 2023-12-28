56d8abc35fcaca0446eef5a290e53d775e45aa6f
Min Kim <minkimcello@gmail.com>
2023-12-27 20:16:23 -0500

    Introduce function to return all items in one array

--- scripts/landscape-yaml-parser/utils/readLandscapeData.ts
+++ scripts/landscape-yaml-parser/utils/readLandscapeData.ts
@@ -1,11 +1,44 @@
 import { parse } from 'yaml';
-import { Landscape } from '../types';
+import {
+  Landscape,
+  Category,
+  LandscapeItem,
+} from '../types';
 
-export async function readLandscapeData(): Promise<Landscape> {
+async function readLandscapeData(): Promise<Landscape> {
   const response = await fetch('https://raw.githubusercontent.com/cncf/landscape/master/landscape.yml');
   if (!response.ok) {
     throw new Error('Failed to fetch landscape data');
   }
   const landscapeFile = await response.text();
-  return parse(landscapeFile);
+  const landscapeData: Landscape  = parse(landscapeFile);
+
+  return landscapeData;
+}
+
+function getItemsFromSubCategory({ item, category }: {
+  item: Category;
+  category: string;
+}) {
+  return item.subcategories.reduce((acc, subcategory) => {
+    return [...acc, ...subcategory.items.map(item => {
+      return {
+        ...item,
+        subcategory: item.name,
+        category,
+      };
+    })];
+  }, [] as LandscapeItem[]);
+}
+
+export async function getAllLandscapeItems(): Promise<LandscapeItem[]> {
+  const landscapeData = await readLandscapeData();
+  const { landscape } = landscapeData;
+  
+  const allItems = landscape.reduce((acc, item) => {
+    const subcategoriesItems = getItemsFromSubCategory({ item, category: item.name });
+    return [...acc, ...subcategoriesItems];
+  }, [] as LandscapeItem[]);
+
+  return allItems;
 }
