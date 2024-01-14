adb45ef6fb3e5c805f4f698428b0ad9491323f5a
Min Kim <minkimcello@gmail.com>
2024-01-14 09:36:44 -0500

    Fetch member count stat from backend

--- plugins/cncf-stats/src/components/Members/Members.tsx
+++ plugins/cncf-stats/src/components/Members/Members.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useAsync } from 'react-use';
+import { configApiRef, useApi } from '@backstage/core-plugin-api';
 
 export const Members = () => {
+  const config = useApi(configApiRef);
+  const backend = config.getString('backend.baseUrl');
+
+  const { value } = useAsync(async () => {
+    try {
+      const stats = await fetch(`${backend}/api/cncf-stats/stats`)
+        .then(response => response.json());
+      return stats.cncfMembersSummary;
+    } catch(e) {
+      let error;
+      if (e instanceof Error) {
+        error = new Error(e.message);
+      }
+      throw error;
+    }
+  });
+
   return (
-    <>members</>
+    <>
+      members count: {value.count}
+    </>
   );
 }
