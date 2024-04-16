2dd623d0cee58bc8db2c7eb95c8b3c64d7aec174
Min Kim <minkimcello@gmail.com>
2024-04-16 19:44:32 -0400

    Add header title to members pie chart?

--- plugins/cncf-stats/src/components/Members/Members.tsx
+++ plugins/cncf-stats/src/components/Members/Members.tsx
@@ -3,12 +3,14 @@ import { CategoryStats } from 'cncf-common';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import { StyledPieChart } from '../Charts/StyledPieChart';
+import CardHeader from '@mui/material/CardHeader';
 
 const Chart = ({stats}: {
   stats: CategoryStats,
 }) => {
   return (
     <Card>
+      <CardHeader title="By Status" />
       <StyledPieChart stats={stats.subcategoryBreakdown["CNCF Members"]} count={stats.count} />
     </Card>
   );
